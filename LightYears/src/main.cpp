9c05fa573692104f0f240591b5393ab468484d46
Dan.White <danieljameswhite1511@gmail.com>
2024-07-21 22:15:12 +0100

    Add sfml library and automate post build commands

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,1 +1,12 @@
-void main(){}
+
+#include <iostream>
+
+#include "SFML/Graphics.hpp"
+
+
+void main()
+{
+
+	sf::RenderWindow render_window{ sf::VideoMode(10000,10000), "My Window" };
+	std::cout << "Hello!";
+}
