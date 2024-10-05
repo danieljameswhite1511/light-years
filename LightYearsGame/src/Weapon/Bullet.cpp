a392530d09a72fd7a0e286b26dea6cb56b926aac
Dan.White <danieljameswhite1511@gmail.com>
2024-10-05 11:08:23 +0100

    Format bullet.cpp

--- LightYearsGame/src/Weapon/Bullet.cpp
+++ LightYearsGame/src/Weapon/Bullet.cpp
@@ -6,32 +6,25 @@ namespace ly
 	Bullet::Bullet(World* world, Actor* owner, const std::string& texturePath, float speed, float damage)
 		:Actor{world, texturePath}
 		,mOwner{owner}	,mSpeed{speed}, mDamage{damage}
+	{}
 
-	{
-	}
-
-	void Bullet::SetSpeed(float newSpeed)
-	{
+	void Bullet::SetSpeed(float newSpeed){
 		mSpeed = newSpeed;
 	}
 
-	void Bullet::SetDamage(float newDamage)
-	{
+	void Bullet::SetDamage(float newDamage)	{
 		mDamage = newDamage;
 	}
 
-	void Bullet::Tick(float deltaTime)
-	{
+	void Bullet::Tick(float deltaTime){
 		Actor::Tick(deltaTime);
 		Move(deltaTime);
 		if(IsActorOutOfWindowBounds()) {
 			Destroy();
 		}
-
 	}
 
-	void Bullet::Move(float deltaTime)
-	{
+	void Bullet::Move(float deltaTime){
 		sf::Vector2f actorForwardDirection = GetActorForwardDirection();
 
 		actorForwardDirection.x *= -1;
@@ -39,6 +32,4 @@ namespace ly
 
 		AddActorLocationOffset(actorForwardDirection * mSpeed * deltaTime);
 	}
-
-	
 }
