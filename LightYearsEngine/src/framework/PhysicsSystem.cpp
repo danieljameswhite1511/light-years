59815cb51d1b7105b428c93c6c3f4f0db5f8e53f
Dan White <dan.white@integrator.solutions>
2024-10-09 13:09:33 +0100

    Implement physics system singleton

--- LightYearsEngine/src/framework/PhysicsSystem.cpp
+++ LightYearsEngine/src/framework/PhysicsSystem.cpp
@@ -1,6 +1,25 @@
 #include "framework\PhysicsSystem.h"
 
+#include <algorithm>
+
+#include "framework/Core.h"
+
 namespace ly{
 
+    unique<PhysicsSystem> PhysicsSystem::physicsSystem{nullptr};
 
+    PhysicsSystem& PhysicsSystem::Get() {
+
+        if(!physicsSystem) {
+            physicsSystem = std::move(unique<PhysicsSystem>{new PhysicsSystem});
+            return *physicsSystem;
+        }
+        return *physicsSystem;
+    }
+
+    PhysicsSystem::PhysicsSystem()
+        : mPhysicsWorld{b2Vec2{0.f, 0.f}}
+        , mPhysicsScale{0.01f}
+    {
+    }
 }
